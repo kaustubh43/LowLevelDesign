81a521f962255d8a01a56c4449164d1a9d699d28
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2024-12-06 21:47:38 +0530

    task2-incomplete

---
+++ src/Labs2/Client.java
@@ -0,0 +1,6 @@
+package Labs2;
+
+public class Client {
+    User obj = new User();
+    obj.setUserName("Kaustubh Ajgaonkar")
+}
--- src/Labs2/User.java
+++ src/Labs2/User.java
@@ -5,18 +5,31 @@ public class User {
     private String name;
     private String contactInfo;
 
+    // Default constructor
     public User(){
-        this.userId = "";
-        this.name = "";
-        this.contactInfo = "";
+        this.userId = generateUserID();
+    }
+
+    // Parameterised constructor.
+    public User(String name, String contactInfo){
+        this.userId = generateUserID();
+        this.name = name;
+        this.contactInfo = contactInfo;
+    }
+
+    // Copy constructors
+    public User(User other){
+        this.userId = generateUserID();
+        this.name = other.name;
+        this.contactInfo = other.contactInfo;
     }
 
     public String getUserName(){
         return this.name;
     }
 
-    public void setUserName(String userId){
-        this.userId = userId;
+    public void setUserName(String userName){
+        this.userId = userName;
     }
 
     public String getContactInfo(){
@@ -25,6 +38,10 @@ public class User {
 
     public void setContactInfo(String contactInfo){
         this.contactInfo = contactInfo;
+    }
+
+    public static String generateUserID(){
+        return "0";
     }
 
 }
