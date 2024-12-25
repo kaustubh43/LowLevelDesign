e33ecd10e8d8834d1fab4d383b65046d7ffb56de
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2024-12-26 00:18:49 +0530

    generics-2

--- src/AdvancedConcepts/Generics/Client.java
+++ src/AdvancedConcepts/Generics/Client.java
@@ -1,4 +1,11 @@
 package AdvancedConcepts.Generics;
 
+
 public class Client {
+    public static void main(String[] args){
+        Pair p = new Pair("Aloha", 56d);
+        System.out.println(p.getFirst() instanceof String);
+
+        Pair.doSomething(5, "5");
+    }
 }
---
+++ src/AdvancedConcepts/Generics/Pair.java
@@ -0,0 +1,32 @@
+package AdvancedConcepts.Generics;
+
+public class Pair<F, S>{
+    private F first;
+    private S second;
+
+    public Pair(F first, S second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public void setFirst(F first) {
+        this.first = first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public void setSecond(S second) {
+        this.second = second;
+    }
+
+    static <X, Y> void doSomething(X first, Y second){
+        System.out.println(first instanceof String);
+        System.out.println(second instanceof String);
+    }
+}
